254aa9356c42fef6acce88314588d98ac9ef076f
syihwamoza <mozakastella26@gmail.com>
2025-10-22 11:21:49 +0700

    Menambahkan endpoint POST /api/mahasiswa untuk menambah data ke tabel biodata

--- index.js
+++ index.js
@@ -39,4 +39,24 @@ app.get('/api/mahasiswa', (req, res) => {
         res.json(results);
     });
 });
+app.post('/api/mahasiswa', (req, res) => {
+    const {nama, alamat, agama } = req.body;
+
+    if (!nama || !alamat || !agama) {
+        return res.status(400).json({message: 'Nama, alamat, dan agama harus diisi'});
+    }
+
+    db.query(
+        'INSERT INTO biodata (nama, alamat, agama) VALUES (?, ?, ?)', 
+        [nama, alamat, agama], 
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({message: 'Database Error'});
+            }
+
+            res.status(201).json({message: 'User created successfully'});
+        }
+    );
+});
 
